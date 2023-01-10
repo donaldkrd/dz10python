9e38b1e42d20134273637914a7adad834cf7ceca
Alexey Utkin <alex_habar@mail.ru>
2023-01-10 22:33:09 +0300

    bot dz

--- tg-bot.py
+++ tg-bot.py
@@ -44,36 +44,10 @@ def send_text(message):
     elif message.text == 'Да или Нет':
         answer = choice(['Да', 'Нет'])
         bot.send_message(message.chat.id, answer)         
-    elif message.text == 'Кинуть кость':
+    elif message.text == 'Кинуть кубик':
         bot.send_message(message.chat.id, f'Вам выпало {(random.randint(1, 6))}')
     else:
         bot.send_message(message.chat.id, 'Данный функционал находится в разработке, таких команд пока не знаю. Лучше кликните на команду /start')
 
 bot.polling(none_stop=True)
 
-
-
-
-
-
-
-
-
-# import telebot
-# from config import TOKEN
-
-# bot = telebot.TeleBot(TOKEN)
-
-# """Команда Старт"""
-# @bot.message_handler(commands=['start'])
-# def welcome(message):
-#     bot.send_message(message.chat.id, 'Добрых вечеров, бродяги')
-
-# @bot.message_handler(content_types=['text'])
-# def send_text(message):
-#     if message.text == 'привет':
-#         bot.send_message(message.chat.id, 'Привет, как дела?')
-#     else:
-#         bot.send_message(message.chat.id, 'Я тебя не понимаю, понимаю только слово "привет"')
-
-# bot.polling(none_stop=True)
